afa1af48d0e8ec2118610bf322913ec7f92f7f5c
abir ben khaled <benkhaledabir27@gmail.com>
2024-05-10 22:39:21 +0100

    tp1+2

---
+++ src/app/carte-visite/carte-visite.model.ts
@@ -0,0 +1,15 @@
+export class CarteVisite {
+    nom: string = ''; // Initialize with an empty string
+    prenom: string = ''; // Initialize with an empty string
+    entreprise: string = ''; // Initialize with an empty string
+    email: string = ''; // Initialize with an empty string
+    telephone: string = ''; // Initialize with an empty string
+
+    constructor() {
+        this.nom = '';
+        this.prenom = '';
+        this.entreprise = '';
+        this.email = '';
+        this.telephone = '';
+    }
+}
